285d8fa1d1d254ddff7f099a41f85a2ae587ec42
minhna1112 <minhngau2804@gmail.com>
2024-04-10 23:09:53 +0700

    try to put useState for site analytics

--- my-website/src/theme/Footer/index.js
+++ my-website/src/theme/Footer/index.js
@@ -1,11 +1,12 @@
 // src/theme/Footer/index.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {useThemeConfig} from '@docusaurus/theme-common';
 import FooterLinks from '@theme/Footer/Links';
 import CustomIconLinks from './CustomIconLinks'; // Your custom component
 import FooterLogo from '@theme/Footer/Logo';
 import FooterCopyright from '@theme/Footer/Copyright';
 import FooterLayout from '@theme/Footer/Layout';
+
 
 function Footer() {
   const {footer} = useThemeConfig();
@@ -14,6 +15,11 @@ function Footer() {
   }
   const {links, logo, style, copyright} = footer;
   
+  // const [visits, setVisits] = useState(0);
+  // const [onlineVisitors, setOnlineVisitors] = useState(0);
+
+  // <script type="text/javascript" id="clstr_globe" src="//clustrmaps.com/globe.js?d=Gs7YlhruXg6hMrapzyex6AsYMEX0u7aumDqxujRjBeA"></script>
+
   return (
     <FooterLayout
       style={style}
@@ -23,5 +29,6 @@ function Footer() {
     />
   );
 }
+
 
 export default React.memo(Footer);
