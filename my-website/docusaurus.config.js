3533241e57a74cda27b2a8372bf66e8c55b647e7
minhna1112 <minhngau2804@gmail.com>
2024-11-28 15:31:47 +0000

    Edit copyright

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} My Personal Page. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Minh Nguyen. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
